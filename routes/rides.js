f5462f963e0b42b5f5deb7425217942709e3baa2
raj gannamaneniE <raj.gannamaneni@skyscanner.net>
2019-11-28 17:41:18 +0800

    feat: add pagination
    Following functionalities implemented :
     -> Pagination
    -> Refactoring

---
+++ routes/rides.js
@@ -0,0 +1,91 @@
+const express = require('express');
+
+const ridesRouter = express.Router();
+const database = require('../utils/db');
+
+ridesRouter.get('/rides', async (req, res) => {
+    try {
+        const {
+            start, limit,
+        } = req.body;
+        if (start === undefined || limit === undefined) {
+            res.status(500).send('Input parameters are incorrect');
+        }
+        // eslint-disable-next-line prefer-template
+        const rows = await database.query('SELECT * FROM Rides LIMIT ' + limit + ' OFFSET ' + start);
+        res.json(rows);
+    } catch (error) {
+        res.send(error.stack || error.message);
+    }
+});
+
+ridesRouter.get('/rides/:id', async (req, res) => {
+    try {
+        const rows = await database.query(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`);
+        res.json(rows);
+    } catch (error) {
+        res.send(error.stack || error.message);
+    }
+});
+
+ridesRouter.post('/rides', async (req, res) => {
+    try {
+        const startLatitude = Number(req.body.start_lat);
+        const startLongitude = Number(req.body.start_long);
+        const endLatitude = Number(req.body.end_lat);
+        const endLongitude = Number(req.body.end_long);
+        const riderName = req.body.rider_name;
+        const driverName = req.body.driver_name;
+        const driverVehicle = req.body.driver_vehicle;
+
+        if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180
+            || startLongitude > 180) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+            });
+        }
+
+        if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+            });
+        }
+
+        if (typeof riderName !== 'string' || riderName.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string',
+            });
+        }
+
+        if (typeof driverName !== 'string' || driverName.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string',
+            });
+        }
+
+        if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string',
+            });
+        }
+
+        const values = [req.body.start_lat, req.body.start_long, req.body.end_lat,
+            req.body.end_long,
+            req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
+
+        const result = await database.save('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) ' +
+            'VALUES (?, ?, ?, ?, ?, ?, ?)', values);
+
+        res.json(result);
+    } catch (error) {
+        res.status(500).send(error.stack || error.message);
+    }
+    return null;
+});
+
+module.exports = ridesRouter;
--- tests/api.test.js
+++ tests/api.test.js
@@ -8,7 +8,7 @@ const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
  describe('API tests', () => {
-   beforeEach((done) => {
+   beforeAll((done) => {
     db.serialize((err) => {
     if (err) {
       return done(err);
@@ -27,4 +27,35 @@ const buildSchemas = require('../src/schemas');
         .expect(200, done);
     });
   });
+     describe('GET /rides/1', () => {
+         it('should return ride', (done) => {
+             request(app)
+                 .get('/rides/1')
+                 .expect(200, done);
+         });
+     });
+     describe('GET /rides', () => {
+         it('should return rides based on pagination', (done) => {
+             request(app)
+                 .get('/rides')
+                 .send({ start: 0, limit: 100 })
+                 .expect(200, done);
+         });
+     });
+     describe('POST /rides', () => {
+         it('should save new ride', (done) => {
+             request(app)
+                 .post('/rides')
+                 .send({
+                     start_lat: 30,
+                     start_long: 60,
+                     end_lat: 70,
+                     end_long: 100,
+                     rider_name: 'rajesh',
+                     driver_name: 'raj',
+                     driver_vehicle: 'toyota',
+                 })
+                 .expect(200, done);
+         });
+     });
 });
